863e13d43b1cf2b1bac251e2687e49ca25ff5c83
Wahab-dev09 <wahabzsec@gmail.com>
2024-08-13 00:52:38 -0600

    Menu Page Added

---
+++ src/Components/Loading.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Loading.css'
+
+const Loading = () => {
+  return (
+    <div className="loading-s">
+<div className="three-body">
+<div className="three-body__dot"></div>
+<div className="three-body__dot"></div>
+<div className="three-body__dot"></div>
+</div>
+    </div>
+  )
+}
+
+export default Loading
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import App from './App.jsx';
+import './index.css'
+import './App.css';
+import Loading from './Components/Loading';
+const Menu = lazy(() => import('./Components/Menu'));
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
     <Routes>
-      <Route path='/' element={<App/>}/>
+    <Route
+          path='/'
+          element={
+            <Suspense fallback={<Loading/>}>
+              <Menu />
+            </Suspense>
+          }
+        />
     </Routes>
     </BrowserRouter>
   </StrictMode>,
